6c0d35399a1f57c59f636ae47edbebab1df8b86f
emeric254 <emeric254@live.fr>
2015-04-12 12:31:37 +0200

    nouveau fichier : Images/cube.png 	modifié :         README.md 	modifié :         test.py

--- README.md
+++ README.md
@@ -3,6 +3,6 @@ ISOT
 
 ```
 position 2D (x et y) de cases selon leur position 3D (X, Y et Z)
-- x = 1/2 * (X + Z)
-- y = 1/4 * (Y + Z)
+- x = TAILLE * 1/2 * (X - Y + OFFSET)
+- y = TAILLE * 1/4 * (X + Y + Z*2 + OFFSET)
 ```
--- test.py
+++ test.py
@@ -12,18 +12,32 @@ if not pygame.mixer:
 
 
 def load_image(name, colorkey=None):
-    fullname = os.path.join('data', name)
+    fullname = os.path.join('Images', name)
     try:
         image = pygame.image.load(fullname)
     except pygame.error:
-        print 'Cannot load image:', name
+        print("Cannot load image:" + name)
         raise SystemExit
     image = image.convert()
     if colorkey is not None:
         if colorkey is -1:
             colorkey = image.get_at((0, 0))
         image.set_colorkey(colorkey, RLEACCEL)
-    return image, image.get_rect()
+    return image
+
+
+def load_sound(name):
+    class NoneSound:
+        def play(self): pass
+    if not pygame.mixer:
+        return NoneSound()
+    fullname = os.path.join('Sons', name)
+    try:
+        sound = pygame.mixer.Sound(fullname)
+    except pygame.error:
+        print('Cannot load sound:' + name)
+        raise SystemExit
+    return sound
 
 
 class PyGameMain:
@@ -39,9 +53,33 @@ class PyGameMain:
         """Create the Screen"""
         self.screen = pygame.display.set_mode((self.width, self.height))
 
+        background = [
+            [
+                [load_image("cube.png", -1), load_image("cube.png", -1), load_image("cube.png", -1)],
+                [load_image("cube.png", -1), load_image("cube.png", -1), load_image("cube.png", -1)],
+                [load_image("cube.png", -1), load_image("cube.png", -1), load_image("cube.png", -1)]
+            ],
+            [
+                [load_image("cube.png", -1), load_image("cube.png", -1), load_image("cube.png", -1)],
+                [load_image("cube.png", -1), load_image("cube.png", -1), load_image("cube.png", -1)],
+                [load_image("cube.png", -1), load_image("cube.png", -1), load_image("cube.png", -1)]
+            ],
+            [
+                [load_image("cube.png", -1), load_image("cube.png", -1), load_image("cube.png", -1)],
+                [load_image("cube.png", -1), load_image("cube.png", -1), load_image("cube.png", -1)],
+                [load_image("cube.png", -1), load_image("cube.png", -1), load_image("cube.png", -1)]
+            ]
+        ]
+
+        for z in range(len(background)):
+            for y in range(len(background[z])):
+                for x in range(len(background[z][y])):
+                    self.screen.blit(background[z][y][x], (140 * 1/2 * (x-y +3), 140 * 1/4 * (x+y-z*2 +3)))
+
     def main_loop(self):
         """This is the Main Loop of the Game"""
         while 1:
+            pygame.display.update()
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
